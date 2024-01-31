db5835c2ef34940008c72f7a37c11899d0b42800
nischalmainali21 <nischalmainali21@gmail.com>
2024-01-31 17:12:09 +0545

    update input types

--- components/InputGroup.tsx
+++ components/InputGroup.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { LoginFormValues } from "@/types";
-import { FieldValues, FieldErrors, UseFormRegister } from "react-hook-form";
+import { FieldErrors, UseFormRegister } from "react-hook-form";
 
 type PropsType = {
   inputType: string;
-  name: string;
+  name: keyof LoginFormValues;
   id: string;
   placeholder?: string;
   labelText: string;
-  register: any;
-  errors: any;
+  register: UseFormRegister<LoginFormValues>;
+  errors: FieldErrors<LoginFormValues>;
 };
 
 const InputGroup = ({
@@ -29,11 +29,12 @@ const InputGroup = ({
       <div className="relative mt-2 rounded-md shadow-sm">
         <input
           type={inputType}
-          name={name}
           id={id}
           className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20  ring-1 ring-inset ring-myColor-300 placeholder:text-myColor-400 focus:ring-2 focus:ring-inset focus:ring-myColor-600 sm:text-sm sm:leading-6"
           placeholder={placeholder}
-          {...register(name, { required: true })}
+          {...register(name, {
+            required: { value: true, message: `${name} is required` },
+          })}
         />
       </div>
       {errors[name] && (
