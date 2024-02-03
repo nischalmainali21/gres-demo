2db4927e914871a0bdbc12372384f1395954308a
nischalmainali21 <nischalmainali21@gmail.com>
2024-02-03 10:02:48 +0545

    updated loading for product detail

--- app/product/[slug]/loading.tsx
+++ app/product/[slug]/loading.tsx
@@ -1,10 +1,15 @@
 import Spinner from "@/components/Spinner";
+import { Skeleton } from "@/components/ui/skeleton";
 import React from "react";
 
 const Loading = () => {
   return (
-    <div className="flex h-screen items-center justify-center ">
-      <Spinner />
+    <div className="grid grid-cols-1 p-2 sm:grid-cols-2 sm:gap-4 sm:p-4 md:p-8">
+      <div>
+        <Skeleton className="h-[350px] w-full rounded-xl sm:h-[460px] xl:h-[500px]" />
+      </div>
+      <Skeleton className="h-4 w-[250px]" />
+      <div></div>
     </div>
   );
 };
