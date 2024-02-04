7dea2f58969d9f5b7f48007242145c7d020de577
nischalmainali21 <nischalmainali21@gmail.com>
2024-02-04 09:07:27 +0545

    added protected route

---
+++ middleware.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from "next/server";
+import { obtain } from "./app/action";
+
+const protectedRoutes = ["/product"];
+
+export default async function middleware(request: NextRequest) {
+  const userToken = await obtain();
+  if (!userToken && request.nextUrl.pathname.startsWith("/product")) {
+    const url = request.nextUrl.clone();
+    url.pathname = "/login";
+    return NextResponse.redirect(url);
+  }
+}
