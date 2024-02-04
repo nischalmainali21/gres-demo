d3d1798e6ad24e141a4edb1a4f7e31ef2344a56b
nischalmainali21 <nischalmainali21@gmail.com>
2024-02-04 09:20:12 +0545

    token expiry check

--- middleware.ts
+++ middleware.ts
@@ -5,9 +5,15 @@ const protectedRoutes = ["/product"];
 
 export default async function middleware(request: NextRequest) {
   const userToken = await obtain();
+  //check for token expiry
+  if (userToken) {
+    const isTokenExpired = isExpired(userToken?.value);
+  }
   if (!userToken && request.nextUrl.pathname.startsWith("/product")) {
     const url = request.nextUrl.clone();
     url.pathname = "/login";
     return NextResponse.redirect(url);
   }
 }
+
+function isExpired(token: string) {}
