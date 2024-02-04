9d7628880807ef63518e864ad0bcd91794db8c92
nischalmainali21 <nischalmainali21@gmail.com>
2024-02-04 20:45:22 +0545

    updated web page navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+import { redirect } from "next/navigation";
 export default function Home() {
+  redirect("/product");
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Test
+      Empty Page
+      <Link href="/product" className="text-2xl font-bold">
+        Go to Product
+      </Link>
     </main>
   );
 }
