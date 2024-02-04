f2dd8d9b9b38ca2275c46dbc2494a72e7d8570e3
nischalmainali21 <nischalmainali21@gmail.com>
2024-02-04 20:24:20 +0545

    update Pagination

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -13,7 +13,7 @@ const Pagination = ({
 }: PropsType) => {
   return (
     <div>
-      <button
+      {/* <button
         onClick={() => handlePageChange(currentPage - 1)}
         disabled={currentPage === 1}
       >
@@ -25,7 +25,18 @@ const Pagination = ({
         disabled={currentPage === totalPages}
       >
         Next
-      </button>
+      </button> */}
+      <div className="flex gap-1">
+        {Array.from({ length: totalPages }, (_, index) => (
+          <button
+            key={index + 1}
+            onClick={() => handlePageChange(index + 1)}
+            className={`relative z-10 inline-flex items-center  rounded-lg px-4 py-2 text-sm font-semibold text-myColor-900 focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-myColor-600  ${currentPage === index + 1 ? "bg-myColor-600 text-stone-100" : "bg-red-100"}`}
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
