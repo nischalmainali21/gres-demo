2807fb53a8b4901bd4a94ec75003f156807426c8
nischalmainali21 <nischalmainali21@gmail.com>
2024-02-04 15:09:19 +0545

    navbar ui done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,6 +7,7 @@ import { FaStore } from "react-icons/fa6";
 import { usePathname } from "next/navigation";
 import { obtain } from "@/app/action";
 import { jwtDecode } from "jwt-decode";
+import Image from "next/image";
 
 type DecodeTokenType = {
   id: number;
@@ -30,15 +31,26 @@ const Navbar = ({ userToken }: PropsType) => {
   //   console.log(userToken);
   const decodedToken: DecodeTokenType = jwtDecode(userToken.value);
   //   console.log(decodedToken);
-  const { username, email } = decodedToken;
+  const { email, image } = decodedToken;
   //   console.log(username, email);
+
+  function handleAvatar() {
+    console.log("image clicked");
+    setOpen((prev) => !prev);
+  }
+
+  function handleLogOut() {
+    console.log("logout the user");
+  }
   return (
     <nav>
       <div
-        className={`relative z-10  h-[60px] w-screen shadow-lg sm:h-[80px] ${!nav ? "" : "mb-10"}`}
+        className={`relative z-10  h-[60px] w-screen shadow-lg sm:h-[80px] ${!nav ? "" : "mb-44"}`}
       >
         <div className="flex h-full w-full items-center justify-between px-2">
-          <FaStore className=" h-8 w-16 text-myColor-600 sm:h-14" />
+          <Link href="/">
+            <FaStore className=" h-8 w-16 text-myColor-600 sm:h-14" />
+          </Link>
           <ul className="hidden items-center gap-x-60 lg:flex">
             <li className=" rounded-lg text-xl">
               <Link
@@ -51,43 +63,49 @@ const Navbar = ({ userToken }: PropsType) => {
 
             <li className="rounded-lg  p-4 text-xl">
               <Link
-                className={`text-xl font-light ${pathname == "/analytics" ? "bg-myColor-600 text-stone-100" : ""}`}
-                href="/analytics"
+                className={`rounded-lg p-2 text-xl font-light ${pathname == "/stats" ? "bg-myColor-600 text-stone-100" : ""}`}
+                href="/stats"
               >
-                ANALYTICS
+                STATS
               </Link>
             </li>
           </ul>
 
           <div className="mx-16 hidden lg:flex">
-            {/* {!user && <div>
-            <Link to="/app/signin">
-            <button className="bg-transparent hover:bg-indigo-600 hover:text-white lg:px-8 py-3 text-black mr-4 rounded-full">
-               Sign In
-            </button>
-            </Link>
-            <Link to="/app/signup">
-            <button className="px-8 py-3 rounded-full">
-                Sign Up
-            </button>
-            </Link>
-            </div>} */}
+            {/* {!userToken && (
+              <div>
+                <Link href="/login">
+                  <button className="mr-4 rounded-full bg-transparent py-3 text-black hover:bg-indigo-600 hover:text-white lg:px-8">
+                    Sign In
+                  </button>
+                </Link>
+              </div>
+            )} */}
 
-            {/* {user&&<div className="ml-10 mr-10 px-8  " onClick={handleAvatar}>
-                <img className="h-[50px] w-[100px]" src={profile} />
+            {userToken && (
+              <div
+                className="relative h-12 w-16 cursor-pointer px-8 "
+                onClick={handleAvatar}
+              >
+                <Image src={image} alt="user profile" fill />
 
-                
-                <ul className={!open ? 'hidden' : 'absolute mt-3  bg-white rounded-2xl px-4 py-4'} >
-                    <div className="text-[#f9a826]">
-                    {userName}
-                    </div>
-                   <a href="http://localhost:3000/app/bookings"> <li className="hover:bg-gray-200 w-full">My Bookings</li></a>
-                    <li onClick={handleLogOut} className="hover:bg-gray-200 w-full">Log Out</li>
-
-
+                <ul
+                  className={
+                    !open
+                      ? "hidden"
+                      : "absolute  right-2  top-2 z-10 mt-6 w-[200px] rounded-2xl bg-white px-4 py-4"
+                  }
+                >
+                  <div className="text-[#474bd8]">{email}</div>
+                  <li
+                    onClick={handleLogOut}
+                    className="w-full hover:bg-gray-200"
+                  >
+                    Log Out
+                  </li>
                 </ul>
-                
-            </div>} */}
+              </div>
+            )}
           </div>
           <div className="block lg:hidden" onClick={handleClick}>
             {!nav ? (
@@ -102,24 +120,33 @@ const Navbar = ({ userToken }: PropsType) => {
           className={
             !nav
               ? "hidden"
-              : "flex h-[60px] w-full flex-col items-center justify-center gap-2 bg-stone-300 px-8 py-8 text-center lg:hidden"
+              : "mt-2 flex h-[150px] w-full flex-col items-center justify-center  gap-4 rounded-lg px-8 py-8 text-center shadow-lg lg:hidden"
           }
         >
+          <li className="flex w-full items-center justify-center gap-4">
+            <div className="relative h-8 w-8">
+              <Image src={image} alt="user profile" fill />
+            </div>
+            <div className="text-[#474bd8]">{email}</div>
+          </li>
           <li className="w-full">
             <Link
               href="/product"
-              className={`text-base font-normal ${pathname == "/product" ? "bg-myColor-600 text-stone-100" : ""}`}
+              className={`rounded-lg bg-myColor-100 p-1 text-base font-normal ${pathname == "/product" ? "bg-myColor-600 text-stone-100" : ""}`}
             >
               Products
             </Link>
           </li>
           <li className="w-full">
             <Link
-              href="/analytics"
-              className={`text-base font-normal ${pathname == "/analytics" ? "bg-myColor-600 text-stone-100" : ""}`}
+              href="/stats"
+              className={`rounded-lg bg-myColor-100 p-1 px-2 text-base  font-normal ${pathname == "/stats" ? "bg-myColor-600 text-stone-100" : ""}`}
             >
-              Analytics
+              Stats
             </Link>
+          </li>
+          <li onClick={handleLogOut} className="w-full hover:bg-myColor-600">
+            Log Out
           </li>
         </ul>
       </div>
